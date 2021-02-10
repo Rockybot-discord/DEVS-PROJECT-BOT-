a950c440c6b80c9786b71e50cbd07c1045aac3f9
RockyBot team <78040515+Rockybot-discord@users.noreply.github.com>
2021-02-10 13:46:22 +0100

    Create index.js
    index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const { token, prefix } = require('./config.json');
+
+const { readdirSync } = require('fs');
+
+const { join } = require('path');
+
+const config = require('./config.json');
+client.config = config;
+
+const db = require('quick.db');
+
+
+
+const { GiveawaysManager } = require('discord-giveaways');
+
+client.giveawaysManager = new GiveawaysManager(client, {
+    storage: "./giveaways.json",
+    updateCountdownEvery: 5000,
+    default: {
+        botsCanWin: false,
+        exemptPermissions: [],
+        embedColor: "#FF0000",
+        reaction: "🎉"
+    }
+});
+
+client.commands = new Discord.Collection();
+const commandFiles = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(join(__dirname, "commands", `${file}`));
+    client.commands.set(command.name, command);
+}
+
+
+client.on("error", console.error);
+
+client.on('ready', () => {
+    console.log('I am ready');
+    client.user.setActivity(`DEVELOPERS SERVER`)
+});
+
+
